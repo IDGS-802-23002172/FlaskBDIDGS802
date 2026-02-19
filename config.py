9fbce4263f3fe3eb942e0a22383bc6e08be115ea
IDGS-802-23002172 <83305@alumnos.utleon.edu.mx>
2026-02-18 20:53:59 -0600

    Conexion a la base de datos

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config(object):
+    SECRET_KEY='Clave nueva'
+    SESSION_COOKIE_SECURE=False
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:1234@127.0.0.1/idgs802'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
---
+++ forms.py
@@ -0,0 +1,20 @@
+from wtforms import Form
+from wtforms import StringField, IntegerField, PasswordField
+from wtforms import EmailField
+from wtforms import validators
+
+class UserForm2(Form):
+    id = IntegerField('id')
+    nombre = StringField('Nombre',[
+        validators.DataRequired(message="Este campo es requerido"),
+        validators.length(min=3, max=10, message="Longitud de 3 a 10 caracteres")
+    ])
+    apaterno = StringField('Apaterno',[
+        validators.DataRequired(message="Este campo es requerido")
+    ])
+    amaterno = StringField('Amaterno',[
+        validators.DataRequired(message="Este campo es requerido")
+    ])
+    correo = EmailField('Correo',[
+        validators.DataRequired(message="Este campo es requerido")
+    ])
