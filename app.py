46be0e2f3cab24b2af85aaf95df17ec0892b2164
IDGS-802-23002172 <83305@alumnos.utleon.edu.mx>
2026-02-19 18:46:50 -0600

    Modificaciones

--- app.py
+++ app.py
@@ -24,7 +24,63 @@ def page_not_found(e):
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    create_form=forms.UserForm2(request.form)
+    #tem = Alumnos.query('select * from alumnos')
+    alumno=Alumnos.query.all()
+    return render_template("index.html", form=create_form, alumnos=alumno)
+
+@app.route("/alumnos", methods=['GET','POST'])
+def alumnos():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'POST':
+        alum = Alumnos(nombre=create_form.nombre.data,
+                apaterno=create_form.apaterno.data,
+                amaterno=create_form.amaterno.data,
+                correo=create_form.correo.data)
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("Alumnos.html",form=create_form)
+
+@app.route("/detalles",methods=['GET','POST'])
+def detalles():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        id=request.args.get('id')
+        nombre=alum1.nombre
+        apaterno=alum1.apaterno
+        amaterno=alum1.amaterno
+        correo=alum1.correo
+    return render_template("detalles.html",form=create_form,id=id,nombre=nombre,apaterno=apaterno,amaterno=amaterno,correo=correo)
+
+@app.route("/modificar",methods=['GET','POST'])
+def modificar():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=request.args.get('id')
+        create_form.nombre.data=alum1.nombre
+        create_form.apaterno.data=alum1.apaterno
+        create_form.amaterno.data=alum1.amaterno
+        create_form.correo.data=alum1.correo
+    if request.method == 'POST':
+        id = create_form.id.data
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum1.id=id
+        alum1.nombre=str.rstrip(create_form.nombre.data)
+        alum1,apaterno=create_form.apaterno.data
+        alum1.amaterno=create_form.amaterno.data
+        alum1.correo=create_form.correo.data
+        db.session.add(alum1)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("modificar.html",form=create_form)
+
+
+
 
 if __name__ == '__main__':
     csrf.init_app(app)
